12f96748e5eae37be50282dfebe36994356b5e79
The Assembly <nikhil@theassembly.ae>
2019-02-23 12:59:13 +0400

    Step 3

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const {app, BrowserWindow} = require("electron");
 const path = require('path');
 const Window = require("./Window")
 
+const ElectronReload = require('electron-reload')
+ElectronReload(__dirname) 
+
 function main(){
     let mainWindow = new Window({file:path.join("web", "index.html")});
 }
