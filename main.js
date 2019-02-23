e57eee4a6b9fe90bb07755c6aebd7e4a900aac64
The Assembly <nikhil@theassembly.ae>
2019-02-23 12:52:35 +0400

    Use Window

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-console.log("From main.js...");
-
 const {app, BrowserWindow} = require("electron");
 const path = require('path');
+const Window = require("./Window")
 
 function main(){
-    let mainWindow = new BrowserWindow({
-        width: 1000, height: 1000
-    });
-    mainWindow.loadFile(path.join("web", "index.html"));
+    let mainWindow = new Window({file:path.join("web", "index.html")});
 }
 
 app.on('ready', main);
+
+app.on('window-all-closed', function () {
+    app.quit()
+})
